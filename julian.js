dc5b9873cb5b10944afbfe0841d8ae7ad54a97fa
Kiril <vatev.1@gmail.com>
2013-06-12 01:28:31 -0400

    added custom julian selection
    plus bug fixes

--- julian.js
+++ julian.js
@@ -5,8 +5,9 @@
 		else if (y%4 === 0) return true;
 		else return false;
 	}
-
-	function getDay(date){
+	
+	
+	function getJulian(date){
 		var day = date.getUTCDate(),
 			month = date.getUTCMonth() + 1, //month starts at 0
 			year = date.getUTCFullYear(),
@@ -28,20 +29,103 @@
 		return doy; // date on egg cartons starts at 1 (or maybe 0)
 	}
 	
-	function setDay(julian, date){
-		var year = date.getFullYear();
+	function getDate(julian){
+		var year = (new Date()).getFullYear();
+		var leap = (isLeap(year)) ? 1:0;
+		var month = 0;
+		var day = 0;
 		
+		switch(true){
+			case julian<=(31):
+				month = 1; //"January";
+				day = julian;
+				break;
+			case julian<=(59 + leap):
+				month = 2; //"February";
+				day = julian - 31;
+				break;
+			case julian<=(90 + leap):
+				month = 3; //"March";
+				day = julian - (59 + leap);
+				break;
+			case julian<=(120 + leap):
+				month = 4; //"April";
+				day = julian - (90 + leap);
+				break;
+			case julian<=(151 + leap):
+				month = 5; //"May";
+				day = julian - (120 + leap);
+				break;
+			case julian<=(181 + leap):
+				month = 6; //"June";
+				day = julian - (151 + leap);
+				break;
+			case julian<=(212 + leap):
+				month = 7; //"July";
+				day = julian - (181 + leap);
+				break;
+			case julian<=(243 + leap):
+				month = 8; //"August";
+				day = julian - (212 + leap);
+				break;
+			case julian<=(273 + leap):
+				month = 9; //"September";
+				day = julian - (243 + leap);
+				break;
+			case julian<=(304 + leap):
+				month = 10; //"October";
+				day = julian - (273 + leap);
+				break;
+			case julian<=(334 + leap):
+				month = 11; //"November";
+				day = julian - (304 + leap);
+				break;
+			default:
+				month = 12; //"December";
+				day = julian - (334 + leap);
+				break;
+		}
+		
+		/*
+		var newDate = new Date();
+		newDate.setDate = day;
+		newDate.setFullYear = year;
+		newDate.setMonth = (month - 1); //month starts at 0
+		/* */
+		
+		var nd = new Date(year+'-'+month+'-'+day);
+		
+		//var UTCDate = new Date( newDate.toDateString() ); //code expects UTC Date
+		
+		//return UTCDate;
+		return nd;
 	}
 	
-	Date.prototype.getJulian = function(){ return getDay(this); }
+	window.getDate = getDate;
+	
+	Date.prototype.getJulian = function(){ return getJulian(this); }
 	
 	var julian = {
 		//vars
-		date: new Date( (new Date()).toDateString() ),
+		date: new Date( (new Date()).toDateString() ), //code expects UTC date
 		julianDOM: document.getElementById("julian"),
 		dateDOM: document.getElementById("date"),
 		
 		//functions
+		changeJulian: function(val){
+			//this.julianDOM.innerHTML = val;
+			console.log(val);
+			
+			this.date = getDate(val);
+			
+			console.log(this.date);
+			this.displayJulian().displayDate();
+			console.log('change done');
+		},
+		changeDate: function(val){
+			this.date = val;
+			this.displayJulian().displayDate();
+		},
 		displayJulian: function(){
 			this.julianDOM.innerHTML = this.date.getJulian();
 			return this;
@@ -86,15 +170,33 @@
 		hideToday();
 	};
 	
+	//select custom date
 	document.getElementById("date").onclick = function(){
 		document.getElementById("dateInput").focus();
 		document.getElementById("dateInput").click();
 	};
 	
 	document.getElementById("dateInput").onchange = function(ev){
-		julian.date = new Date(ev.target.value);
-		julian.displayJulian().displayDate();
+		document.getElementById("dateInput").click(); //hack necessary for immediate update
+		julian.changeDate( new Date(ev.target.value) );
+		this.value = null;
 		
 		showToday();
 	};
+	
+	//select custom julian
+	document.getElementById("julian").onclick = function(){
+		document.getElementById("julianInput").focus();
+		document.getElementById("julianInput").click();
+	};
+	
+	document.getElementById("julianInput").oninput = function(ev){
+		//julian.changeJulian(Number(ev.target.valueAsNumber));
+		julian.changeJulian(Number(this.value));
+		
+		showToday();
+	};
+	document.getElementById("julianInput").onchange = function(){
+		this.value = null;
+	}
 })();
